99e0e4007228ec9e418fc3dbb5caa8c4525d6561
Lucas Sandoz <sandozlucas1995@gmail.com>
2020-05-24 17:41:19 -0300

    Updates

---
+++ precache-manifest.0a7ab3563aaf2ba59b9012504f610739.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e27f5eb08450e891f26b735afd1654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb931fdc4c96968fe6a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "72e93401380d11fe778f",
+    "url": "/static/js/2.1a7e1d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a7e1d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb931fdc4c96968fe6a",
+    "url": "/static/js/main.18b1844c.chunk.js"
+  },
+  {
+    "revision": "42f54bafafca3f5841d0",
+    "url": "/static/js/runtime-main.6e7cdcd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
