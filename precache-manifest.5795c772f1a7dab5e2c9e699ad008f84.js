8d49555ccd75787bbb79601ecc0bbe48fd0d6389
Lucas Sandoz <sandozlucas1995@gmail.com>
2020-05-25 18:09:53 -0300

    Updates

---
+++ precache-manifest.5795c772f1a7dab5e2c9e699ad008f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df07a1ccadaeeb8c10818f75de8560e",
+    "url": "/personal-resume/index.html"
+  },
+  {
+    "revision": "9381790358e58bf6bbbb",
+    "url": "/personal-resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8679dd35933f930a8341",
+    "url": "/personal-resume/static/js/2.f0417f49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-resume/static/js/2.f0417f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9381790358e58bf6bbbb",
+    "url": "/personal-resume/static/js/main.27fafa3a.chunk.js"
+  },
+  {
+    "revision": "a08973dddfe5d9046be8",
+    "url": "/personal-resume/static/js/runtime-main.0b839a76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
